d01d4377b86491289f5da1acb793fb332c7cd02d
Oscar Roni <125587006+O-RONI@users.noreply.github.com>
2023-05-11 14:27:44 -0600

    Add files via upload

--- apptaller.py
+++ apptaller.py
@@ -3,11 +3,7 @@ from tkinter import messagebox, filedialog
 
 
 root = Tk()
-root.geometry('480x380')
-root.config(bg= "#f52a2a")
-root.iconbitmap(bitmap='correo.ico')
-root.resizable(0,0)
-root.title('Correos')
+root.title("Mi Aplicación")
 
 
 checkbox_var = BooleanVar()
@@ -16,74 +12,83 @@ checkbox_var.set(True)
 
 def guardar_datos():
     
-   
+    filename = filedialog.asksaveasfilename(defaultextension=".txt")
+    if filename:
        
-    nombre = nombre_entry.get()
-    apellido = apellido_entry.get()
-    correo = correo_entry.get()
-    
+        nombre = nombre_entry.get()
+        apellido = apellido_entry.get()
+        correo = correo_entry.get()
         
-    file = open("correos.txt", "a")
-    file.write("Nombre: {}\n".format(nombre))
-    file.write("Apellido: {}\n".format(apellido))
-    file.write("Correo electrónico: {}\n".format(correo))
-    
-    if checkbox_var.get():
-        file.write("Acepta recibir correos electrónicos\n")
-    else:
-        file.write("No acepta recibir correos electrónicos\n")
-    file.close()
-       
-    messagebox.showinfo("Datos guardados", "Los datos se han guardado correctamente.")
+        with open(filename, "w") as f:
+            f.write("Nombre: {}\n".format(nombre))
+            f.write("Apellido: {}\n".format(apellido))
+            f.write("Correo electrónico: {}\n".format(correo))
+            if checkbox_var.get():
+                f.write("Acepta recibir correos electrónicos\n")
+            else:
+                f.write("No acepta recibir correos electrónicos\n")
+        
+        messagebox.showinfo("Datos guardados", "Los datos se han guardado correctamente.")
 
 
 menu_bar = Menu(root)
 root.config(menu=menu_bar)
 
-
+# Menu
 archivo_menu = Menu(menu_bar)
 menu_bar.add_cascade(label="Archivo", menu=archivo_menu)
 archivo_menu.add_command(label="Guardar", command=guardar_datos)
 
-
-campos_marco = LabelFrame(root, text="Datos Personales",font=("Bauhaus 93",14))
+# 
+campos_marco = LabelFrame(root, text="Datos Personales")
 campos_marco.pack(padx=10, pady=10)
 
-
-nombre_label = Label(campos_marco, text="Nombre",font=("Bauhaus 93",10))
+nombre_label = Label(campos_marco, text="Nombre")
 nombre_label.grid(row=0, column=0, padx=5, pady=5)
 nombre_entry = Entry(campos_marco)
 nombre_entry.grid(row=0, column=1, padx=5, pady=5)
 
-apellido_label = Label(campos_marco, text="Apellido",font=("Bauhaus 93",10))
+apellido_label = Label(campos_marco, text="Apellido")
 apellido_label.grid(row=1, column=0, padx=5, pady=5)
 apellido_entry = Entry(campos_marco)
 apellido_entry.grid(row=1, column=1, padx=5, pady=5)
 
-correo_label = Label(campos_marco, text="Correo electrónico",font=("Bauhaus 93",10))
+
+#Radio buttons
+def seleccionGenero():
+   selection = "You selected the option " + str(genero.get())
+   label.config(text = selection)
+
+
+genero = StringVar()
+label = Label(campos_marco, text = "Genero")
+
+generoM_boton = Radiobutton(campos_marco, text="Option 1", variable=genero, value="Masculino", command=seleccionGenero)
+generoM_boton.grid(row=5, column=0, padx=5, pady=5)
+
+generoF_boton = Radiobutton(campos_marco, text="Option 2", variable=genero, value="Femenino", command=seleccionGenero)
+generoM_boton.grid(row=5, column=0, padx=5, pady=5)
+
+generoO_boton = Radiobutton(campos_marco, text="Option 3", variable=genero, value="Otro", command=seleccionGenero)
+generoM_boton.grid(row=5, column=0, padx=5, pady=5)
+
+
+# label correo
+correo_label = Label(campos_marco, text="Correo electrónico")
 correo_label.grid(row=2, column=0, padx=5, pady=5)
 correo_entry = Entry(campos_marco)
 correo_entry.grid(row=2, column=1, padx=5, pady=5)
 
 
-edad_label = Label(campos_marco, text="Edad",font=("Bauhaus 93",10))
-edad_label.grid(row=3, column=0, padx=5, pady=5)
-edad_spinbox = Spinbox(campos_marco, from_=0, to=120)
-edad_spinbox.grid(row=3, column=1, padx=5, pady=5)
-
-
-
 checkbox = Checkbutton(campos_marco, text="Acepto recibir correos electrónicos", variable=checkbox_var)
-checkbox.grid(row=4, columnspan=2, padx=5, pady=5)
-
+checkbox.grid(row=3, columnspan=2, padx=5, pady=5)
 
 
 guardar_boton = Button(root, text="Guardar", command=guardar_datos)
 guardar_boton.pack(padx=10, pady=10)
 
 
-
-
+root.mainloop()
 
 
 
